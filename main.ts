d9ff934b6d03713f749f8bfd35169d81be57f0a2
burgii8 <95907553+burgii8@users.noreply.github.com>
2021-12-10 09:38:57 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,10 +3,10 @@ input.onGesture(Gesture.Shake, function () {
     numero = randint(1, 6)
 })
 basic.forever(function () {
-    basic.showNumber(numero)
-})
-basic.forever(function () {
     if (numero == 6) {
         basic.showIcon(IconNames.Happy)
     }
+})
+basic.forever(function () {
+    basic.showNumber(numero)
 })
